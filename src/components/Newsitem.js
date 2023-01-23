d577ca94b07995e74287f12a9e2e433c1eb4710a
Taraansh <taraanshnandwani123@gmail.com>
2023-01-23 16:04:20 +0530

    Used props to show data in data item

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
 
 export class Newsitem extends Component {
-
   render() {
-    let { title, description, imageUrl, newsUrl} = this.props;
+    let { title, description, imageUrl, url } = this.props;
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={!imageUrl?"https://pbs.twimg.com/profile_images/768075575273611264/3IQo-8g3_400x400.jpg":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsdetails" className="btn btn-sm btn-primary">
+            <a
+              href={url}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-sm btn-primary"
+            >
               Read More
             </a>
           </div>
