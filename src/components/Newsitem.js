9ec06de1722e06e7658ab3544d52147967910da0
Taraansh <taraanshnandwani123@gmail.com>
2023-01-24 23:21:20 +0530

    Added author, date and source

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -2,22 +2,17 @@ import React, { Component } from "react";
 
 export class Newsitem extends Component {
   render() {
-    let { title, description, imageUrl, url } = this.props;
+    let { title, description, imageUrl, url, author, publishedAt, source } = this.props;
     return (
       <div className="my-3">
         <div className="card">
+        <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}>{source}</span>
           <img src={!imageUrl?"https://pbs.twimg.com/profile_images/768075575273611264/3IQo-8g3_400x400.jpg":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a
-              href={url}
-              target="_blank"
-              rel="noreferrer"
-              className="btn btn-sm btn-dark"
-            >
-              Read More
-            </a>
+            <p class="card-text"><small class="text-muted">By {!author?"Unknown":author} on {new Date(publishedAt).toTimeString()}</small></p>
+            <a href={url} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
           </div>
         </div>
       </div>
