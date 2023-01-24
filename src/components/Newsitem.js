5dde3184f4c52a08b30780750b9627db6b30c429
Taraansh <taraanshnandwani123@gmail.com>
2023-01-24 15:31:22 +0530

    Removed card width

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -5,7 +5,7 @@ export class Newsitem extends Component {
     let { title, description, imageUrl, url } = this.props;
     return (
       <div className="my-3">
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card">
           <img src={!imageUrl?"https://pbs.twimg.com/profile_images/768075575273611264/3IQo-8g3_400x400.jpg":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
