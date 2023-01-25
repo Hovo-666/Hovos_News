074acb4269905c903bf2d0e3128e0fa005c8bc37
Taraansh <taraanshnandwani123@gmail.com>
2023-01-25 22:54:23 +0530

    Modified badge

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -6,7 +6,9 @@ export class Newsitem extends Component {
     return (
       <div className="my-3">
         <div className="card">
-        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}>{source}</span>
+          <div style={{display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: '0'}}>
+        <span className="badge rounded-pill bg-danger">{source}</span>
+        </div>
           <img src={!imageUrl?"https://pbs.twimg.com/profile_images/768075575273611264/3IQo-8g3_400x400.jpg":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
