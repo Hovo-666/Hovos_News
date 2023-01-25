9ae5d8321af5b68b8ed15469576ae3ea0512f4aa
Taraansh <taraanshnandwani123@gmail.com>
2023-01-25 16:02:09 +0530

    Correction

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -6,12 +6,12 @@ export class Newsitem extends Component {
     return (
       <div className="my-3">
         <div className="card">
-        <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}>{source}</span>
+        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}>{source}</span>
           <img src={!imageUrl?"https://pbs.twimg.com/profile_images/768075575273611264/3IQo-8g3_400x400.jpg":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <p class="card-text"><small class="text-muted">By {!author?source:author} on {new Date(publishedAt).toTimeString()}</small></p>
+            <p className="card-text"><small className="text-muted">By {!author?source:author} on {new Date(publishedAt).toTimeString()}</small></p>
             <a href={url} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
           </div>
         </div>
