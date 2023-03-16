21ce4e641ab5f9105cbdb2284b06d6fbd7e8c1c2
Hovo-666 <hovhannes.shahazizyan051@gmail.com>
2023-03-16 22:40:32 +0400

    tryings

--- src/App.js
+++ src/App.js
@@ -24,15 +24,15 @@ const App = () => {
         progress={progress}/>
 
         <Routes>
-        <Route exact path="/" element={<News setProgress={setProgress} apiKey={apiKey}  key="general" pageSize={pageSize} country="us" category="general" />} />
-        {/* <Route path="/general" element={<News setProgress={setProgress} apiKey={apiKey}  pageSize={pageSize} country="in" category="general" />} /> */}
-        <Route exact path="/business" element={<News setProgress={setProgress} apiKey={apiKey}  key="business" pageSize={pageSize} country="us" category="business" />} />
-        <Route exact path="/entertainment" element={<News setProgress={setProgress} apiKey={apiKey}  key="entertainment" pageSize={pageSize} country="us" category="entertainment" />} />
-        <Route exact path="/health" element={<News setProgress={setProgress} apiKey={apiKey}  key="health" pageSize={pageSize} country="us" category="health" />} />
-        <Route exact path="/science" element={<News setProgress={setProgress} apiKey={apiKey}  key="science" pageSize={pageSize} country="us" category="science" />} />
-        <Route exact path="/sports" element={<News setProgress={setProgress} apiKey={apiKey}  key="sports" pageSize={pageSize} country="us" category="sports" />} />
-        <Route exact path="/technology" element={<News setProgress={setProgress} apiKey={apiKey}  key="technology" pageSize={pageSize} country="us" category="technology" />} />
-        </Routes>
+          <Route exact path="/" element={<News setProgress={setProgress} apiKey={apiKey}  key="general" pageSize={pageSize} country="us" category="general" />} />
+          {/* <Route path="/general" element={<News setProgress={setProgress} apiKey={apiKey}  pageSize={pageSize} country="in" category="general" />} /> */}
+          <Route exact path="/business" element={<News setProgress={setProgress} apiKey={apiKey}  key="business" pageSize={pageSize} country="us" category="business" />} />
+          <Route exact path="/entertainment" element={<News setProgress={setProgress} apiKey={apiKey}  key="entertainment" pageSize={pageSize} country="us" category="entertainment" />} />
+          <Route exact path="/health" element={<News setProgress={setProgress} apiKey={apiKey}  key="health" pageSize={pageSize} country="us" category="health" />} />
+          <Route exact path="/science" element={<News setProgress={setProgress} apiKey={apiKey}  key="science" pageSize={pageSize} country="us" category="science" />} />
+          <Route exact path="/sports" element={<News setProgress={setProgress} apiKey={apiKey}  key="sports" pageSize={pageSize} country="us" category="sports" />} />
+          <Route exact path="/technology" element={<News setProgress={setProgress} apiKey={apiKey}  key="technology" pageSize={pageSize} country="us" category="technology" />} />
+          </Routes>
         </Router>
       </div>
     );
--- src/components/News.js
+++ src/components/News.js
@@ -15,19 +15,21 @@ const News = (props) => {
   };
 
   const updateNews = async () => {
-    props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
-    setLoading(true);
-    const data = await fetch(url);
-    props.setProgress(30);
-    const parsedData = await data.json();
-    props.setProgress(70);
+    setTimeout(async () => {
+      props.setProgress(10);
+      const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+      setLoading(true);
+      const data = await fetch(url);
+      props.setProgress(30);
+      const parsedData = await data.json();
+      props.setProgress(70);
 
-    setArticles(parsedData.articles)
-    setLoading(false)
-    setTotalResults(parsedData.totalResults)
+      setArticles(parsedData.articles)
+      setLoading(false)
+      setTotalResults(parsedData.totalResults)
 
-    props.setProgress(100);
+      props.setProgress(100);
+    } , 10000)
   }
 
   useEffect(() => {
